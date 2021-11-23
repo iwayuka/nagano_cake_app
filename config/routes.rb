6a3f9df53fe9deda672821ccee5602130304e659
EC2 Default User <ec2-user@ip-172-31-40-180.ec2.internal>
2021-11-23 08:05:17 +0000

    [Update]ジャンルのルーティングをonlyを使用して指定

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 }
 
 namespace :admin do
-    resources :genres, :except => [:new]
+    resources :genres, only: [:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
