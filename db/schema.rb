b8f9c7bf3b146d3b73b52deff937c48b1f19a35b
EC2 Default User <ec2-user@ip-172-31-40-180.ec2.internal>
2021-11-23 00:14:30 +0000

    [Add]モデル「Cart_item」の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_000416) do
+ActiveRecord::Schema.define(version: 2021_11_23_001141) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2021_11_23_000416) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "customer_id"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,6 +59,12 @@ ActiveRecord::Schema.define(version: 2021_11_23_000416) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
