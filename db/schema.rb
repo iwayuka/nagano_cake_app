d3b5610204d2536aaa86f08025907d4c569deed2
EC2 Default User <ec2-user@ip-172-31-40-180.ec2.internal>
2021-11-23 00:23:37 +0000

    [Add]モデル「Order_detail」の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_001506) do
+ActiveRecord::Schema.define(version: 2021_11_23_002036) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 2021_11_23_001506) do
     t.text "introduction"
     t.integer "price"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "order_id"
+    t.integer "item_id"
+    t.integer "price"
+    t.integer "amount"
+    t.integer "making_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
