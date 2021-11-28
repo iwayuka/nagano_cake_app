ff51e93f8c6c3c0b6e2350018fd08861505fbc29
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-28 20:38:26 +0000

    [Add]商品一覧画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,9 +5,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    # １. データを新規登録するためのインスタンス作成
+    item = Item.new(item_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    item.save
+    # ３. 商品詳細画面へリダイレクト ※データの保存がうまくできるか確認のため、一度一覧画面へリダイレクト
+    redirect_to admin_items_path
   end
 
   def index
+    # itemデータベースから情報を取得
+    @items = Item.all
   end
 
   def show
@@ -18,4 +26,11 @@ class Admin::ItemsController < ApplicationController
 
   def destroy
   end
+
+  private
+  # ストロングパラメータ
+  def item_params
+  params.require(:item).permit(:genre_id, :name, :image_id, :introduction, :price, :is_active)
+end
+
 end
--- db/migrate/20211123000416_create_items.rb
+++ db/migrate/20211123000416_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :image_id
       t.text :introduction
       t.integer :price
-      t.boolean :is_activea
+      t.boolean :is_active, null: false
 
       t.timestamps
     end
