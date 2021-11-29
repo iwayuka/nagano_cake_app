cc9c8f4a14e49ac1be7bb0c33e698d0b0595b86b
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-29 22:39:48 +0000

    [Add]画像詳細画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,9 +19,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def destroy
