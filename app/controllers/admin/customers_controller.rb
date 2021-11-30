fdc42c31c80df0fbc708231c09a14e5ee775756a
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-30 01:06:27 +0000

    [Add]会員一覧画面の作成

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,7 @@
+class Admin::CustomersController < ApplicationController
+  
+  def index
+    @customers = Customer.all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
 namespace :admin do
     resources :genres, only: [:index, :create, :edit, :update]
     resources :items
+    resources :customers
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
