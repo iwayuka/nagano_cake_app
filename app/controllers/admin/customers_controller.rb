f34fbb18f6fafa5a902dc2a695470e9528130c5b
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-30 02:17:14 +0000

    [Add]会員情報編集画面の追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,15 @@
 class Admin::CustomersController < ApplicationController
-  
+
   def index
     @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
   end
 
 end
