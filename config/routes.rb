7f8418332626e330e9b2bc9ee6118e42b49b49d3
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-30 06:00:40 +0000

    [Add]注文履歴詳細(金額計算を除く）の追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ namespace :admin do
     resources :items
     resources :customers
     root :to => 'homes#top'
+    resources :order, only: [:show]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
