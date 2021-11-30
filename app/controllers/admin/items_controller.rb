6441e9b2be5859998ff294c066299489818afcf7
iwayuka <iwabuchi.yuka07@gmail.com>
2021-11-30 00:17:38 +0000

    [Add]商品編集updateメソッドの追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -26,6 +26,13 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to admin_item_path(item.id)
+  end
+
+
   def destroy
   end
 
