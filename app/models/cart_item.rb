16ed06c9cbf997ae05b8face32b02b1c0b8b6e3e
iwayuka <iwabuchi.yuka07@gmail.com>
2021-12-24 23:30:02 +0000

    [Add]カート 機能に関連するモデルに記述

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,6 @@
 class CartItem < ApplicationRecord
+
+    belongs_to :customer
+    has_many :items, dependent: :destroy
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :cart_item, dependent: :destroy
          
   # is_deletedがfalseならtrueを返すようにしている
   def active_for_authentication?
