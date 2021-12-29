2495f8eb18620b5d469dfe9c6aee711505c5c394
iwayuka <iwabuchi.yuka07@gmail.com>
2021-12-29 07:11:35 +0000

    [Fix]ショッピングカートの削除機能の不具合を修正

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,8 +11,9 @@ class CartItemsController < ApplicationController
   end
 
   def destroy
-    cart_item = CartItem.find(params[:id])
-    cart_item.destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
   end
 
   def destroy_all
