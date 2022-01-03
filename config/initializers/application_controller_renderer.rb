be9b591d0664947c8efb462dd4ab14c3f44f7356
iwayuka <iwabuchi.yuka07@gmail.com>
2022-01-03 15:30:52 +0000

    [Fix]商品詳細画面の写真を表示させる

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0e520075575225ebeeb6816d194e7441db3ce0cb93def957889075b8e3f59701271293866b17b5d7b0075d7604a2f84aa603b65c9a84d5cea47412dffe234a66'
