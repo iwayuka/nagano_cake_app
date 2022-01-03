e0696777ecf60a7109c97a00da3f3dcde96ec749
iwayuka <iwabuchi.yuka07@gmail.com>
2022-01-03 18:01:16 +0000

    [Add]トップ画面のレイアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,8 +15,8 @@ class ItemsController < ApplicationController
     # １. データを新規登録するためのインスタンス作成
     cart_item = CartItem.new(cart_item_params)
     # ２. データをデータベースに保存するためのsaveメソッド実行
-    if CartItem.find_by(item_id: params[:cart_item][:item_id])
-      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
       redirect_to cart_items_path
