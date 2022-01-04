19cd7b9ba16bf4afe7537f90377aa36c22228ea1
iwayuka <iwabuchi.yuka07@gmail.com>
2022-01-04 05:12:48 +0000

    [Add]商品一覧レイアウト追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_customer!,except: [:index, :show]
 
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page]).per(8)
+    @items_count = Item.all
   end
 
   def show
